a7e61f64eb06c702a9e9349f55a49e101fad1e18
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:09 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Salary Calculator by Calculator.iO
+Plugin Name: Salary Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/salary-calculator/
 Description: This free salary calculator easily switches between hourly, bi-weekly, monthly, and yearly pay. Holidays and vacation days need special consideration and adjustments.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Salary Calcula
 
 function display_ci_salary_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/salary-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Salary Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_salary_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Salary Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_salary_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_salary_calculator', 'display_ci_salary_calculator' );
